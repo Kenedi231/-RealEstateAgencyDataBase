85f33cd5077e25e929cddfd92288d32b3b11e56a
Nikolai Kalsin <kalsinkolya@gmail.com>
2019-12-19 03:53:47 +0300

    change user

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const config = {
     port: 3000,
     portDatabase: 5432,
-    userDatabase: 'rlowen',
-    passwordDatabase: '123321',
+    userDatabase: 'guest',
+    passwordDatabase: '123456',
     databaseName: 'rea',
     hostDatabase: 'localhost',
     secret: 'mysecretkey',
